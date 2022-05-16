09607fbf1f9489a5b586fa299dc608c633b753c6
Emmanuel Eni <eejeba@gmail.com>
2022-05-16 20:24:42 +0100

    I just simply tried out the destructuring approach of requiring the 'createServer' method from http.

--- my1stServer.js
+++ my1stServer.js
@@ -1,11 +1,22 @@
 // 19:44 mon may16 wk20 2022
 // My first attempt at creating a server.
 
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-    console.log(`client has made a request.`)
-    res.end(`You're welcomed to my home page.`)
+// const server = http.createServer((req, res) => {
+//     console.log(`client has made a request.`)
+//     res.end(`You're welcomed to my home page.`)
+// })
+
+// server.listen(4433)
+
+
+
+const {createServer} = require('http')
+
+const server = createServer((req, res) => {
+    console.log(`An http request from client has been received.`)
+    res.end(`You're warmly, warmly welcomed to my home page!`)
 })
 
 server.listen(4433)
